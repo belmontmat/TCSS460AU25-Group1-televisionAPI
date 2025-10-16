764f5e9be4ca1a2bf08df1746c98a112df25db20
matbelmont <belmontmat@gmail.com>
2025-10-15 22:51:27 -0700

    Added some basic swagger documentation and a /api-docs route to view it

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -19,4 +19,18 @@ routes.get('/', (request, response) => {
     });
 });
 
+routes.get('/shows', (request, response) => {
+    // Replace this with query and formating of data from db
+    response.json({
+        success: true,
+        message: 'If you\'re reading this message, the server is working!',
+        version: '0.0.1',
+        timestamp: new Date().toISOString(),
+        endpoints: {
+            
+        },
+        documentation: 'Not available yet'
+    });
+});
+
 export default routes;
