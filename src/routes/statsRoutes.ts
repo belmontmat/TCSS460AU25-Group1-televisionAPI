986f9d23d124f47f9e2f391e62d4bcad90845a92
matbelmont <belmontmat@gmail.com>
2025-10-25 18:34:39 -0700

    added the rest of the stats routes

--- src/routes/statsRoutes.ts
+++ src/routes/statsRoutes.ts
@@ -1,7 +1,7 @@
 /**
  * Routes for getting quick aggrigations and stats
  */
-import { getGenreStats, getNetworkStats } from '@/controller/statsRoutesController';
+import { getActorStats, getCompanyStats, getCountryStats, getGenreStats, getNetworkStats, getStatusStats, getYearStats } from '@/controller/statsRoutesController';
 import { Router } from 'express';
 
 const statsRoutes = Router();
@@ -23,6 +23,76 @@ statsRoutes.get('/networks', async(request, response) => {
     try {
 
         const result = await getNetworkStats();
+        
+        return response.json(result);
+        
+    } catch (error) {
+        return response.status(500).json({
+            error: 'Internal server error: ' + error
+        });
+    }
+});
+
+statsRoutes.get('/actors', async(request, response) => {
+    try {
+
+        const result = await getActorStats();
+        
+        return response.json(result);
+        
+    } catch (error) {
+        return response.status(500).json({
+            error: 'Internal server error: ' + error
+        });
+    }
+});
+
+statsRoutes.get('/years', async(request, response) => {
+    try {
+
+        const result = await getYearStats();
+        
+        return response.json(result);
+        
+    } catch (error) {
+        return response.status(500).json({
+            error: 'Internal server error: ' + error
+        });
+    }
+});
+
+statsRoutes.get('/countries', async(request, response) => {
+    try {
+
+        const result = await getCountryStats();
+        
+        return response.json(result);
+        
+    } catch (error) {
+        return response.status(500).json({
+            error: 'Internal server error: ' + error
+        });
+    }
+});
+
+statsRoutes.get('/status', async(request, response) => {
+    try {
+
+        const result = await getStatusStats();
+        
+        return response.json(result);
+        
+    } catch (error) {
+        return response.status(500).json({
+            error: 'Internal server error: ' + error
+        });
+    }
+});
+
+statsRoutes.get('/companies', async(request, response) => {
+    try {
+
+        const result = await getCompanyStats();
         
         return response.json(result);
         
